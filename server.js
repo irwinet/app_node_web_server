0f656920dc10ed4ea09258ee27e53d652aea7693
Irwin <irwinestradatorres@gmail.com>
2021-01-18 09:41:03 -0500

    Add Handlebars

--- server.js
+++ server.js
@@ -3,16 +3,23 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/', (req, res) => {
-//     // res.send('Hola Mundo');
-//     let salida = {
-//         nombre: 'Irwin',
-//         edad: 25,
-//         url: req.url
-//     };
+// Express HBS Engine
+app.set('view engine', 'hbs');
 
-//     res.send(salida);
-// });
+app.get('/', (req, res) => {
+    // res.send('Hola Mundo');
+    // let salida = {
+    //     nombre: 'Irwin',
+    //     edad: 25,
+    //     url: req.url
+    // };
+
+    // res.send(salida);
+    res.render('home', {
+        nombre: 'Irwin',
+        anio: new Date().getFullYear()
+    });
+});
 
 app.listen(3000, () => {
     console.log('Escuchando perticiones en el puerto 3000');
