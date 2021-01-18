9b7866d3ad21287550008e1db0a6096a2a8e6325
Irwin <irwinestradatorres@gmail.com>
2021-01-18 09:08:51 -0500

    Servir Content Static

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    // res.send('Hola Mundo');
-    let salida = {
-        nombre: 'Irwin',
-        edad: 25,
-        url: req.url
-    };
+app.use(express.static(__dirname + '/public'));
 
-    res.send(salida);
-})
+// app.get('/', (req, res) => {
+//     // res.send('Hola Mundo');
+//     let salida = {
+//         nombre: 'Irwin',
+//         edad: 25,
+//         url: req.url
+//     };
+
+//     res.send(salida);
+// });
 
 app.listen(3000, () => {
     console.log('Escuchando perticiones en el puerto 3000');
-})
+});
