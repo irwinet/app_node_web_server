a474dad131b2726a86d325ac5ea52fe326501b9d
Irwin <irwinestradatorres@gmail.com>
2021-01-18 10:04:08 -0500

    Usando Parciales HBS

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var hbs = require('hbs');
 
 app.use(express.static(__dirname + '/public'));
 
 // Express HBS Engine
+hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
@@ -19,6 +21,10 @@ app.get('/', (req, res) => {
         nombre: 'Irwin',
         anio: new Date().getFullYear()
     });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
 });
 
 app.listen(3000, () => {
