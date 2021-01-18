93a1a9bdd53654798edaab671fae13e8c8258ca1
Irwin <irwinestradatorres@gmail.com>
2021-01-18 10:15:36 -0500

    Usando Helpers

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 var hbs = require('hbs');
+require('./hbs/helpers');
 
 app.use(express.static(__dirname + '/public'));
 
 // Express HBS Engine
 hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
+
+
 
 app.get('/', (req, res) => {
     // res.send('Hola Mundo');
@@ -18,8 +21,7 @@ app.get('/', (req, res) => {
 
     // res.send(salida);
     res.render('home', {
-        nombre: 'Irwin',
-        anio: new Date().getFullYear()
+        nombre: 'irwin'
     });
 });
 
