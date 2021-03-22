8432456ed4c6837343a1cbf7654dc5bded1476f0
Irwin <irwinestradatorres@gmail.com>
2021-03-22 12:41:15 -0500

    Download CSV

--- app-old.js
+++ app-old.js
@@ -3,6 +3,12 @@ const http = require('http');
 
 http.createServer((req, res) => {
 
+        //res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+        //res.writeHead(200, { 'Content-Type': 'application/csv' });
+        // res.write('id, nombre\n');
+        // res.write('1, Irwin\n');
+        // res.end();
+
         res.writeHead(200, { 'Content-Type': 'application/json' })
         let salida = {
             nombre: 'Irwin',
